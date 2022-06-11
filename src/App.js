38a01eba676cad4541f6df9acc97fbac1214bec1
Jaslan <Jaslan.dlnv@gmail.com>
2022-06-11 20:33:33 +0500

    refactor: Cart and add adaptive

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
         setItems(data);
         setIsLoading(false);
       });
+      window.scrollTo(0, 0);
   }, []);
 
   return (
